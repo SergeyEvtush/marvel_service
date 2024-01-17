89185ab47319bb6672216187d94c3798c83fbd39
SergeyEvtush <evtushru208@gmail.com>
2024-01-18 00:27:47 +0200

    созданы глоб перем с адресом и ключом написаны метода получения и трансформации данных

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,15 +1,35 @@
+class MarvelService {
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=086d0cbebc54f84aaf54ebf4685baf20";
+  getResourse = async (url) => {
+    let res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url},status ${res.status}`);
+    }
+    return await res.json();
+  };
+  getAllCharacters = async () => {
+    const res = await this.getResourse(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
+    return res.data.results.map(this._transformCharacter);
+  };
 
+  getCharacter = async (id) => {
+    const res = await this.getResourse(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+    return this._transformCharacter(res.data.results[0]);
+  };
 
-class MarvelService { 
-	  getResourse=async(url)=> {
-		let res = await fetch(url);
-		if (!res.ok) { 
-			throw new Error(`Could not fetch ${url},status ${res.status}`)
-		}
-		return await res.json();
-	}
-	getAllCharacters = () => { 
-		return this.getResourse("https://gateway.marvel.com:443/v1/public/characters?apikey=086d0cbebc54f84aaf54ebf4685baf20");
-	}
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
+  };
 }
-export default MarvelService
+export default MarvelService;
