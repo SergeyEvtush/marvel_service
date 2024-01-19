9e3665a1471a6d00f9755c728f911bdfccfa2045
SergeyEvtush <evtushru208@gmail.com>
2024-01-19 12:13:08 +0200

    повесил обновление персонажа на кнопку try it.сделал получение данных на charList

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,70 @@
 import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import { Component } from 'react';
+import MarvelService from '../../services/MarvelService';
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 
-const CharList = () => {
-    return (
+
+class CharList extends Component {
+	constructor(props) { 
+		super(props)
+		this.state = {
+			charList: [],
+			loading: true,
+			error:false
+		};
+		this.updateCharList();
+	}
+	marvelService = new MarvelService();
+	onCharLoaded = (charList) => {
+		this.setState({ charList,loading:false });
+	  };
+	updateCharList = () => {
+		 this.marvelService
+			 .getAllCharacters()
+			 .then(this.onCharLoaded)
+			 .catch(this.onError);
+	 };
+	 onError = () => {
+		this.setState({
+			loading: false,
+			error: true
+		});
+	 }
+	renderCharList = (charArray) => {
+		/**char__item_selected */
+		const cahrs = charArray.map((el) => {
+			return (
+				<li className="char__item" key={el.id }>
+					<img src={el.thumbnail} alt={ el.name} />
+					<div className="char__name">{ el.name}</div>
+				</li>)
+		});
+		return (
+			<ul className="char__grid">
+				{ cahrs}
+			</ul>
+		)
+	 }
+
+	render() {
+		const {
+			charList,loading,error
+		 } = this.state;
+		 const errorMessage = error ? <ErrorMessage /> : null;
+		 const spinner = loading ? <Spinner /> : null;
+		const content = !(loading || error) ? this.renderCharList(charList) : null;
+		
+		return (
         <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
+				{errorMessage}
+			 {spinner}
+			 { content}
             <button className="button button__main button__long">
                 <div className="inner">load more</div>
             </button>
         </div>
-    )
+    )}
 }
 
 export default CharList;
